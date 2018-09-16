cc07ddfcfc508cb7b2bdb9c8762fd4c816d1903c
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-09-16 22:09:42 +0300

    2nd try

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 class Sorter {
 
-    constructor(element, index, indices) {
+    constructor(element, index, indices, compareFunction) {
         this.element = element;
         this.index = index;
         this.indices = indices;
         this.array = [];
+        this.compareFunction = compareFunction;
+
 
     }
 
@@ -34,32 +36,29 @@ class Sorter {
     }
 
     sort(indices) {
+        if (this.compareFucntion === undefined) {
+            indices.sort();
+            for (var k = 0; k < this.array.length; k++) {
 
-
-        for (var k = 0; k < this.array.length; k++) {
-            var wasSwap = false;
-            for (var i = 0; i < indices.length - 1; i++) {
-
-                if (this.array[indices[i]] > this.array[indices[i + 1]]) {
-                    var swaps = this.array[indices[i]];
-                    this.array[indices[i]] = this.array[indices[i + 1]];
-                    this.array[indices[i + 1]] = swaps;
-                    wasSwap = true;
-
-
+                for (var i = 0; i < indices.length - 1; i++) {
+                    {
+                        if (this.array[indices[i]] > this.array[indices[i + 1]]) {
+                            var swaps = this.array[indices[i]];
+                            this.array[indices[i]] = this.array[indices[i + 1]];
+                            this.array[indices[i + 1]] = swaps;
+                        }
+                    }
                 }
-
             }
-            if (!wasSwap) break;
+
         }
-
-
-
-
+        else {
+            this.array.sort(setComparator);
+        }
 
     }
     setComparator(compareFunction) {
-
+        this.array.sort(compareFunction);
         // your implementation
     }
 }
