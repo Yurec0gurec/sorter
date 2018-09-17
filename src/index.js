da38ca7a51cc1a4052522b8c42324fcad9e4f0b6
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-09-17 12:05:05 +0300

    3rd try

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 class Sorter {
 
-    constructor(element, index, indices, compareFunction) {
+    constructor(element, index, indices) {
         this.element = element;
         this.index = index;
         this.indices = indices;
+        this.metod = 1;
         this.array = [];
-        this.compareFunction = compareFunction;
-
 
     }
 
@@ -36,31 +35,40 @@ class Sorter {
     }
 
     sort(indices) {
-        if (this.compareFucntion === undefined) {
-            indices.sort();
+            
+        indices.sort();
+        if (this.metod) {
+
             for (var k = 0; k < this.array.length; k++) {
 
                 for (var i = 0; i < indices.length - 1; i++) {
-                    {
-                        if (this.array[indices[i]] > this.array[indices[i + 1]]) {
-                            var swaps = this.array[indices[i]];
-                            this.array[indices[i]] = this.array[indices[i + 1]];
-                            this.array[indices[i + 1]] = swaps;
-                        }
+
+                    if (this.array[indices[i]] > this.array[indices[i + 1]]) {
+                        var swaps = this.array[indices[i]];
+                        this.array[indices[i]] = this.array[indices[i + 1]];
+                        this.array[indices[i + 1]] = swaps;
                     }
                 }
             }
-
-        }
-        else {
-            this.array.sort(setComparator);
+        } else {
+             var helpArr = [];
+            for (var i = 0; i < indices.length; i++) {
+                helpArr[helpArr.length] = this.array[indices[i]];
+            }
+            helpArr.sort(this.comparator);
+            for (var i = 0; i < indices.length; i++) {
+                this.array[indices[i]] = helpArr[i];
+            }
         }
 
     }
+
     setComparator(compareFunction) {
-        this.array.sort(compareFunction);
+
+        this.comparator = compareFunction;
+        this.metod = 0;
         // your implementation
     }
-}
 
-module.exports = Sorter;
+}
+    module.exports = Sorter;
